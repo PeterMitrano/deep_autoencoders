1087d97ddcb954dae80c62c90d84eae62befaf1e
Peter Mitrano <mitranopeter@gmail.com>
2017-05-12 23:57:23 -0400

    add classification layer
    tensorboard log-dir: log_data/May_12/May_12_23:57:22/

--- main.py
+++ main.py
@@ -27,30 +27,30 @@ def generate_image_and_label_batch(image, label, min_queue_examples, batch_size)
 
         tf.summary.image('images', images)
 
-        return images, tf.reshape(label_batch, [batch_size])
+        return images, tf.one_hot(tf.squeeze(label_batch), depth=NUM_CLASSES)
 
 
 class Model:
-    def __init__(self, images, global_step, batch_size):
+    def __init__(self, image_batch, label_batch, global_step, batch_size):
         self.trainers = []
         self.losses = []
 
-        self.flat_images = tf.reshape(images, [-1, img_dim], name='flatten')
+        self.image_batch_flat = tf.reshape(image_batch, [-1, img_dim], name='flatten')
 
         with tf.name_scope('layer_1'):
-            self.h1_dim = 100
+            self.h1_dim = 500
             self.w1 = tf.Variable(tf.truncated_normal([img_dim, self.h1_dim], 0.0, 0.1), name='w1')
             self.w1_trans = tf.transpose(self.w1, [1, 0])
             self.w1_viz = tf.reshape(self.w1_trans, [-1, IMAGE_SIZE, IMAGE_SIZE, N_CHANNELS], name='w1_viz')
             self.b1 = tf.Variable(tf.constant(0.05, shape=[self.h1_dim]), name='b1')
             self.a1 = tf.Variable(tf.constant(0.05, shape=[img_dim]), name='a1')
-            self.h1 = tf.nn.sigmoid(tf.matmul(self.flat_images, self.w1) + self.b1, name='h1')
+            self.h1 = tf.nn.sigmoid(tf.matmul(self.image_batch_flat, self.w1) + self.b1, name='h1')
             self.y1 = tf.nn.sigmoid(tf.matmul(self.h1, self.w1_trans) + self.a1, name='y1')
-            self.y1_images = tf.reshape(self.y1, [-1, IMAGE_SIZE, IMAGE_SIZE, N_CHANNELS], name='y1_images')
+            self.y1_image_batch = tf.reshape(self.y1, [-1, IMAGE_SIZE, IMAGE_SIZE, N_CHANNELS], name='y1_images')
             self.vars1 = [self.w1, self.b1, self.a1]
 
-            self.reconstruction_loss1 = tf.nn.l2_loss(self.y1 - self.flat_images, name='reconstruction_loss1')
-            self.loss1 = tf.nn.l2_loss(self.y1 - self.flat_images, name='loss1')
+            self.reconstruction_loss1 = tf.nn.l2_loss(self.y1 - self.image_batch_flat, name='reconstruction_loss1')
+            self.loss1 = tf.nn.l2_loss(self.y1 - self.image_batch_flat, name='loss1')
             self.train1 = tf.train.AdamOptimizer(0.002).minimize(self.loss1, global_step, self.vars1, name='train1')
             self.losses.append(self.loss1)
             self.trainers.append(self.train1)
@@ -62,10 +62,10 @@ class Model:
             # tf.summary.histogram('h1', self.h1)
             # tf.summary.histogram('y1', self.y1)
             # tf.summary.image('w1_viz', self.w1_viz, max_outputs=100)
-            tf.summary.image('y1_images', self.y1_images, max_outputs=10)
+            tf.summary.image('y1_images', self.y1_image_batch, max_outputs=10)
 
         with tf.name_scope('layer_2'):
-            self.h2_dim = 10
+            self.h2_dim = 100
             self.w2 = tf.Variable(tf.truncated_normal([self.h1_dim, self.h2_dim], 0.0, 0.1), name='w2')
             self.w2_trans = tf.transpose(self.w2, [1, 0])
             self.b2 = tf.Variable(tf.constant(0.05, shape=[self.h2_dim]), name='b2')
@@ -73,10 +73,10 @@ class Model:
             self.h2 = tf.nn.sigmoid(tf.matmul(self.h1, self.w2) + self.b2, name='h2')
             self.h1_ = tf.nn.sigmoid(tf.matmul(self.h2, self.w2_trans) + self.a2, name='h2_')
             self.y2 = tf.nn.sigmoid(tf.matmul(self.h1_, self.w1_trans) + self.a1, name='y2')
-            self.y2_images = tf.reshape(self.y2, [-1, IMAGE_SIZE, IMAGE_SIZE, N_CHANNELS], name='y2_images')
+            self.y2_image_batch = tf.reshape(self.y2, [-1, IMAGE_SIZE, IMAGE_SIZE, N_CHANNELS], name='y2_images')
             self.vars2 = [self.w2, self.b2, self.a2]
 
-            self.reconstruction_loss2 = tf.nn.l2_loss(self.y2 - self.flat_images, name='reconstruction_loss2')
+            self.reconstruction_loss2 = tf.nn.l2_loss(self.y2 - self.image_batch_flat, name='reconstruction_loss2')
             self.loss2 = tf.nn.l2_loss(self.h1_ - self.h1, name='loss2')
             self.train2 = tf.train.AdamOptimizer(0.002).minimize(self.loss2, global_step, self.vars2, name='train2')
             self.losses.append(self.loss2)
@@ -88,9 +88,25 @@ class Model:
             # tf.summary.histogram('a2', self.a2)
             # tf.summary.histogram('h2', self.h2)
             # tf.summary.histogram('y2', self.y2)
-            tf.summary.image('y2_images', self.y1_images, max_outputs=10)
+            tf.summary.image('y2_images', self.y1_image_batch, max_outputs=10)
 
-        tf.summary.image('images', images, max_outputs=10)
+        with tf.name_scope("layer_3"):
+            self.w3 = tf.Variable(tf.truncated_normal([self.h2_dim, NUM_CLASSES], 0.0, 0.1), name='w3')
+            self.b3 = tf.Variable(tf.constant(0.1, shape=[NUM_CLASSES]), name='b3')
+            self.y3 = tf.matmul(self.h2, self.w3) + self.b3
+            self.pred = tf.argmax(self.y3, axis=1)
+            self.accuracy = tf.reduce_mean(tf.cast(tf.equal(self.pred, tf.argmax(label_batch, axis=1)), dtype=tf.float32))
+            self.vars3 = [self.w3, self.b3]
+
+            self.loss3 = tf.nn.softmax_cross_entropy_with_logits(logits=self.y3, labels=label_batch)
+            self.train3 = tf.train.AdamOptimizer(0.001).minimize(self.loss3)
+            self.losses.append(self.loss3)
+            self.trainers.append(self.train3)
+
+            tf.summary.scalar('classification_loss', self.loss3)
+            tf.summary.scalar('accuracy', self.accuracy)
+
+        tf.summary.image('images', image_batch, max_outputs=10)
 
 
 def main():
@@ -121,7 +137,7 @@ def main():
     min_fraction_of_examples_in_queue = 0.4
     min_queue_examples = int(NUM_EXAMPLES_PER_EPOCH_FOR_TRAIN * min_fraction_of_examples_in_queue)
 
-    images, labels = generate_image_and_label_batch(processed_image, label, min_queue_examples, batch_size)
+    image_batch, label_batch = generate_image_and_label_batch(processed_image, label, min_queue_examples, batch_size)
 
     sess = tf.Session()
 
@@ -138,7 +154,7 @@ def main():
 
     global_step = tf.Variable(0, trainable=False, name='global_step')
 
-    m = Model(images, global_step, batch_size)
+    m = Model(image_batch, label_batch, global_step, batch_size)
 
     init = tf.global_variables_initializer()
     summaries = tf.summary.merge_all()
@@ -153,10 +169,10 @@ def main():
 
     sess.run(init)
 
-    layer_schedule = [2000, 2000]
+    layer_schedule = [2000, 2000, 500]
     layer = 0
     layer_it = 0
-    for i in range(4000):
+    for i in range(4500):
         if layer_it == layer_schedule[layer]:
             layer += 1
             layer_it = 0
